a0d4a1c99e92368bac06821525f24749e89add90
xHadi3 <hadiessam9@gmail.com>
2024-11-04 12:59:55 +0300

    Initial Commit

---
+++ project2/project2App.swift
@@ -0,0 +1,17 @@
+//
+//  project2App.swift
+//  project2
+//
+//  Created by Hadi Al zayer on 02/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
